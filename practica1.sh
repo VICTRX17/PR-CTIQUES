5f8cfa7eec05bdfcbe1ceaba068699bd74679b33
k <k@MacBook-Pro-de-Darius.local>
2024-10-07 10:02:08 +0200

    PART 4

--- practica1.sh
+++ practica1.sh
@@ -1,12 +1,17 @@
 #practica1
 
-awk -F',' '{(1, )}
+
+#PART 1
+cut -d',' -f1-14 --complement "supervivents.csv" > "temp.csv"
 
 
+#PART 2
 awk -F',' '{if ($14 == "False") print $0}' temp.csv > temp2csv
 
 
+#PART 3
+awk -F',' '{if ($8 < 1000000) print $0 ",Bo"}' temp2.csv >> temp3.csv
+awk -F',' '{if (($8 > 1000000) and ($8 < 10000000)) print $0 ",Excel·lent"}' temp2.csv >> temp3.csv
+awk -F',' '{if ($8 > 10000000) print $0 ",Estrella"}' temp2.csv >> temp3.csv
 
-awk -F',' '{if ($8 < 1000000) print $0 ",Bo"}' temp2.csv > temp3.csv
-awk -F',' '{if (($8 > 1000000) and ($8 < 10000000)) print $0 ",Excel·lent"}' temp2.csv > temp3.csv
-awk -F',' '{if ($8 > 10000000) print $0 ",Estrella"}' temp2.csv > temp3.csv
+#PART 4

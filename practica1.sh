a9b937649588e0317b0867ceb679d32adb761772
k <k@MacBook-Pro-de-Darius.local>
2024-10-07 10:23:17 +0200

    COMMIT FINS A PART 4

--- practica1.sh
+++ practica1.sh
@@ -7,7 +7,10 @@ cut -d',' -f1-14 --complement "supervivents.csv" > "temp.csv"
 
 #PART 2
 awk -F',' '{if ($14 == "False") print $0}' temp.csv > temp2csv
-
+linies_originals=$(wc -l temp.csv)
+linies_noves=$(wc -l temp2.csv)
+resta=$((linies_originals - linies_noves))
+echo $resta
 
 #PART 3
 awk -F',' '{if ($8 < 1000000) print $0 ",Bo"}' temp2.csv >> temp3.csv

88bb3fe2c4703d91517d745903303776c362f8aa
k <k@MacBook-Pro-de-Darius.local>
2024-10-07 09:53:18 +0200

    ADD SCRIPT

--- practica1.sh
+++ practica1.sh
@@ -1,3 +1,12 @@
 #practica1
 
-awk -F',' '{if ($14 == False) print $0}' temp.csv > temp2.csv
+awk -F',' '{(1, )}
+
+
+awk -F',' '{if ($14 == "False") print $0}' temp.csv > temp2csv
+
+
+
+awk -F',' '{if ($8 < 1000000) print $0 ",Bo"}' temp2.csv > temp3.csv
+awk -F',' '{if (($8 > 1000000) and ($8 < 10000000)) print $0 ",Excel·lent"}' temp2.csv > temp3.csv
+awk -F',' '{if ($8 > 10000000) print $0 ",Estrella"}' temp2.csv > temp3.csv
